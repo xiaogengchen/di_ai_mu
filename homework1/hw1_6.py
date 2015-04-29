243b839591f998c21045b4bf797b1ed4482e0012
XiaoGengchen <xiao_gengchen@163.com>
2015-04-29 17:00:53 +0800

    作业第七题没做完

--- homework1/hw1_6.py
+++ homework1/hw1_6.py
@@ -13,6 +13,17 @@
 '''
 
 if __name__ == "__main__" :
-    for num in range(1,8):
-        for i in range(1,num):
-            pass
+    while True :
+        NUM = raw_input("请输入一个数字：")
+        if not NUM.isdigit() :
+            continue
+        NUM = int(NUM)
+        break
+    for num in range(1,NUM+1):
+        temp_NUM = num
+        while temp_NUM > 1 :
+            print temp_NUM,
+            temp_NUM -= 1
+        for i in range(1,num+1):
+            print i,
+        print
